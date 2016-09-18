ef11851ede0c895fc7c171fed930257c770ddf52
Yuhao Zhang <zzyzxd@gmail.com>
2016-09-18 00:45:56 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # applechecker
-Check Apple Store Inventory
+
+> Check Apple Store Inventory
+
+Keep checking Apple Store inventory and send you alert when nearby store have your desired device in stock.
+Also let you know if inventory becomes zero again so you don't jump out of bed when it is already too late.
+
+* [Prerequisites](#prerequisites)
+* [Usage](#usage)
+
+## Prerequisites
+
+* `pip install requests`
+
+* A gmail account if want email alert.
+
+* For SMS alert, the script uses the free service provided by [TextBelt](http://textbelt.com/), which has some limitations like "IP addresses are limited to 75 texts per day" and "Phone numbers are limited to 3 texts every 3 minutes". Check their page for details.
+
+## Usage
+
+```
+python stock.py <model> <zipcode> <emails or phone numbers delimited by comma> <check interval in seconds> <your gmail account if you want email alerts> <your gmail password if you want email alerts>
+```
+
+###Example:
+
+Every 5 seconds, check availability of `Apple Watch Stainless Steel Case with White Sport Band` near zipcode 22102 and send email alert to `recipient@example.com` using gmail account `sender@gmail.com`.
+
+```
+python /path/to/stock.py "MNPR2LL/A" "22102" "recipient@example.com" 5 sender@gmail.com sender_password
+```
+
+Every 10 seconds, check availability of `iPhone 7 Plus T-Mobile Jet Black 128GB` near zipcode 22102 and send sms alert to `2021234567`.
+
+```
+python /path/to/stock.py "MN5L2LL/A" "22102" "2021234567" 10
+```
+
+Model number is a unique identifier, U.S. models end with "LL/*". (https://www.theiphonewiki.com/wiki/Model_Regions)
+
+To find out the model number for a specific product:
+
+* Apple Watch: model number hides in query string in URL of the item page.
+
+    Example:
+    `http://www.apple.com/shop/buy-watch/apple-watch/silver-stainless-steel-stainless-steel-sport-band?preSelect=true&product=`**`MNPR2LL/A`**`&step=detail#`
+
+* iPhone: inspect the item page and look for a request to `http://www.apple.com/shop/delivery-message?`
+
+    Example:
+    `http://www.apple.com/shop/delivery-message?parts.0=`**`MN5L2LL%2FA`**`&cppart=TMOBILE%2FUS&_=1474171709609`
+
+For a complete list: http://www.everyi.com/
+
+
---
+++ stock.py
@@ -0,0 +1,75 @@
+import time
+import sys
+import smtplib
+import requests
+
+URL = "http://www.apple.com/shop/retail/pickup-message"
+BUY = "http://store.apple.com/xc/product/"
+
+def check_stock(model, zip_, dest, sec=5, login=None, pwd=None):
+    good_stores = []
+    my_alert = Alert(dest, login, pwd)
+    initmsg = "{0} start tracking {1} in {2}. Alert will sent to {3}".format(
+        time.strftime("%m/%d/%Y %H:%M:%S"), model, zip_, dest)
+    print initmsg
+    my_alert.send(initmsg)
+    params = {'parts.0': model,
+              'location': zip_}
+
+    while True:
+        print "=================================="
+        print "[{current}] Avaiable: {stores}".format(
+            current=time.strftime("%m/%d/%Y %H:%M:%S"),
+            stores=', '.join([store.encode('utf-8') for store in good_stores])
+                    if good_stores else "None")
+
+        stores = requests.get(URL, params=params).json()['body']['stores'][:8]
+        for store in stores:
+            sname = store['storeName']
+            item = store['partsAvailability'][model]['storePickupProductTitle']
+            if store['partsAvailability'][model]['pickupDisplay'] \
+                        == "available":
+                if sname not in good_stores:
+                    good_stores.append(sname)
+                    msg = "Gooo!! {store} has {item}!! {buy}{model}".format(
+                        store=sname, item=item, buy=BUY, model=model)
+                    print "{0} {1}".format(time.strftime("%m/%d/%Y %H:%M:%S"),
+                                           msg)
+                    my_alert.send(msg)
+            else:
+                if sname in good_stores:
+                    good_stores.remove(sname)
+                    msg = "Oops all {item} in {store} are gone :( ".format(
+                        item=item, store=sname)
+                    print "{0} {1}".format(time.strftime("%m/%d/%Y %H:%M:%S"),
+                                           msg)
+                    my_alert.send(msg)
+        time.sleep(int(sec))
+
+class Alert(object):
+    def __init__(self, dest, login=None, password=None):
+        self.dest = dest
+        if login and password:
+            self.login = login
+            self.password = password
+            self.send = self.send_email
+        else:
+            self.send = self.send_sms
+
+    def send_email(self, msgbody):
+        message = "From: {0}\nTo: {1}\nSubject: {2}\n\n{3}".format(
+            self.login, self.dest, "Apple Stock Alert", msgbody)
+        mailer = smtplib.SMTP('smtp.gmail.com:587')
+        mailer.ehlo()
+        mailer.starttls()
+        mailer.ehlo()
+        mailer.login(self.login, self.password)
+        mailer.sendmail(self.login, self.dest, message)
+        mailer.close()
+
+    def send_sms(self, message):
+        r = requests.post('http://textbelt.com/text', data={
+            'number': self.dest, 'message': message})
+
+if __name__ == '__main__':
+    check_stock(*sys.argv[1:])
