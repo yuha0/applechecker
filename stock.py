03216fbc5989005297b23c096e13e949104ba1d1
Yuhao Zhang <zzyzxd@gmail.com>
2016-09-20 09:30:18 -0400

    handle socket.gaierror and more meaningful error messages

--- stock.py
+++ stock.py
@@ -2,31 +2,37 @@
 import time
 import sys
 import smtplib
+from socket import gaierror
 import requests
 
+# only tested for US stores and US text message
 URL = "http://www.apple.com/shop/retail/pickup-message"
 BUY = "http://store.apple.com/xc/product/"
+SMS = "http://textbelt.com/text"
 
-def check_stock(model, zip_, dest, sec=5, login=None, pwd=None):
+def check_stock(model, zipcode, dest, sec=5, login=None, pwd=None):
     good_stores = []
     my_alert = Alert(dest, login, pwd)
     initmsg = "{0} start tracking {1} in {2}. Alert will sent to {3}".format(
-        time.strftime("%m/%d/%Y %H:%M:%S"), model, zip_, dest)
+        time.strftime("%m/%d/%Y %H:%M:%S"), model, zipcode, dest)
     print initmsg
     my_alert.send(initmsg)
     params = {'parts.0': model,
-              'location': zip_}
+              'location': zipcode}
 
     while True:
-        print "=================================="
-        print "[{current}] Avaiable: {stores}".format(
-            current=time.strftime("%m/%d/%Y %H:%M:%S"),
-            stores=', '.join([store.encode('utf-8') for store in good_stores])
-                    if good_stores else "None")
+        time.sleep(int(sec))
+        if good_stores:
+            print "=================================="
+            print "[{current}] Avaiable: {stores}".format(
+                current=time.strftime("%m/%d/%Y %H:%M:%S"),
+                stores=', '.join([s.encode('utf-8') for s in good_stores])
+                        if good_stores else "None")
 
         try:
-            stores = requests.get(URL, params=params).json()['body']['stores'][:8]
-        except (ValueError, KeyError):
+            stores = requests.get(URL, params=params) \
+                    .json()['body']['stores'][:8]
+        except (ValueError, KeyError, gaierror):
             print "Bad response from server..."
             continue
         for store in stores:
@@ -36,7 +42,7 @@ def check_stock(model, zip_, dest, sec=5, login=None, pwd=None):
                         == "available":
                 if sname not in good_stores:
                     good_stores.append(sname)
-                    msg = "Gooo!! {store} has {item}!! {buy}{model}".format(
+                    msg = "Go!! {store} has {item}!! {buy}{model}".format(
                         store=sname, item=item, buy=BUY, model=model)
                     print "{0} {1}".format(time.strftime("%m/%d/%Y %H:%M:%S"),
                                            msg)
@@ -49,7 +55,6 @@ def check_stock(model, zip_, dest, sec=5, login=None, pwd=None):
                     print "{0} {1}".format(time.strftime("%m/%d/%Y %H:%M:%S"),
                                            msg)
                     my_alert.send(msg)
-        time.sleep(int(sec))
 
 class Alert(object):
     def __init__(self, dest, login=None, password=None):
@@ -64,7 +69,11 @@ class Alert(object):
     def send_email(self, msgbody):
         message = "From: {0}\nTo: {1}\nSubject: {2}\n\n{3}".format(
             self.login, self.dest, "Apple Stock Alert", msgbody)
-        mailer = smtplib.SMTP('smtp.gmail.com:587')
+        try:
+            mailer = smtplib.SMTP('smtp.gmail.com:587')
+        except gaierror:
+            print "Couldn't reach Gmail server"
+            return
         mailer.ehlo()
         mailer.starttls()
         mailer.ehlo()
@@ -73,8 +82,17 @@ class Alert(object):
         mailer.close()
 
     def send_sms(self, message):
-        r = requests.post('http://textbelt.com/text', data={
-            'number': self.dest, 'message': message})
+        try:
+            response = requests.post(SMS, data={
+                'number': self.dest, 'message': message}).json()
+            if response['success'] is not True:
+                print response['message']
+                sys.exit(2)
+        except gaierror:
+            print "Couldn't reach TextBelt"
+        if response.json()['success'] is not True:
+            print "Failed to send SMS"
+            sys.exit(2)
 
 if __name__ == '__main__':
     check_stock(*sys.argv[1:])
